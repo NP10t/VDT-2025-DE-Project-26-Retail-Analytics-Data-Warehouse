7e9cf52e2f3f5d87453b5cc8f09ab0b3ec6a4073
NP10t <NP10t@gmail.com>
2025-05-25 15:54:27 +0700

    chore: snapshot code after successful Spark-ClickHouse connector setup

--- data_pipeline/run_etl.py
+++ data_pipeline/run_etl.py
@@ -2,9 +2,21 @@
 from data_pipeline import ( 
                            ingest_data_to_minio, 
                            extract_from_minio, 
+                           transform_retail_data,
+                           load_to_clickhouse,
                         )
 from data_pipeline.utils.minio_utils import init_minio_client
 from data_pipeline.utils.spark_utils import init_spark
+from data_pipeline.utils.clickhouse_utils import (
+    init_clickhouse_client,
+    create_clickhouse_tables,
+)
+
+import logging
+logger = logging.getLogger(__name__)
+from dotenv import load_dotenv
+import os
+
 
 
 def main():
@@ -19,17 +31,44 @@ def main():
 
     # Get MinIO bucket name
     _, bucket_name = init_minio_client()
+    
+    
+    spark.conf.set("spark.sql.catalog.clickhouse", "com.clickhouse.spark.ClickHouseCatalog")
+    spark.conf.set("spark.sql.catalog.clickhouse.host", os.getenv("CLICKHOUSE_HOST", "127.0.0.1"))
+    spark.conf.set("spark.sql.catalog.clickhouse.protocol", os.getenv("CLICKHOUSE_PROTOCOL", "http"))
+    spark.conf.set("spark.sql.catalog.clickhouse.http_port", os.getenv("CLICKHOUSE_HTTP_PORT", "8123"))
+    spark.conf.set("spark.sql.catalog.clickhouse.user", os.getenv("CLICKHOUSE_USER", "default"))
+    spark.conf.set("spark.sql.catalog.clickhouse.password", os.getenv("CLICKHOUSE_PASSWORD", "123456"))
+    spark.conf.set("spark.sql.catalog.clickhouse.database", os.getenv("CLICKHOUSE_DB", "default"))
+    spark.conf.set("spark.clickhouse.write.format", os.getenv("CLICKHOUSE_WRITE_FORMAT", "json"))
+    spark.conf.set("spark.clickhouse.option.http_version", "1.1")
+    spark.conf.set("spark.clickhouse.option.http_client_impl", "apache4")
+
+    logger.info(f"ClickHouse Host: {os.getenv('CLICKHOUSE_HOST')}")
+    logger.info(f"ClickHouse User: {os.getenv('CLICKHOUSE_USER')}")
+    logger.info(f"ClickHouse Database: {os.getenv('CLICKHOUSE_DB')}")
+    
+    
+    data = [("2023-01-01", 1), ("2023-01-02", 2)]
+    df = spark.createDataFrame(data, ["date", "value"])
+    # spark.sql("CREATE TABLE clickhouse.vdtdatabase.test_table (date String, value Int32) ENGINE = MergeTree() ORDER BY date")
+    df.writeTo("clickhouse.vdtdatabase.test_table").append()
 
     # Extract data from MinIO
-    df = extract_from_minio(spark, bucket_name, "raw/retail.csv")
+    # df = extract_from_minio(spark, bucket_name, "raw/retail.csv")
     
-    df.show()  # Display the DataFrame for debugging
+    # df.show()  # Display the DataFrame for debugging
+
+    # clickhouse_client = init_clickhouse_client()
+
+    # # Create ClickHouse tables
+    # create_clickhouse_tables(clickhouse_client)
 
     # # Transform data
-    # transformed_df = transform_data(df)
+    # dim_date, dim_product, dim_customer, fact_sales = transform_retail_data(df)
 
-    # # Load transformed data to MinIO
-    # load_to_minio(spark, transformed_df, bucket_name, "processed/sales_by_product.parquet")
+    # # Load data into ClickHouse
+    # load_to_clickhouse(spark, dim_date, dim_product, dim_customer, fact_sales)
 
     # Stop Spark session
     spark.stop()
---
+++ data_pipeline/utils/clickhouse_utils/__init__.py
@@ -0,0 +1,4 @@
+from .clickhouse_utils import (
+    init_clickhouse_client,
+    create_clickhouse_tables,
+)
